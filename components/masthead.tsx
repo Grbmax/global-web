38e47a90326a2f7f7b38a83e2195ca10355b1bcf
Parziwal <yash5201>
2022-09-06 15:31:00 +0530

    About Us

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 
 const Masthehad: React.FC = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="min-h-screen flex flex-col items-center justify-center -z-50">
+           <h1 className="bg-white z-10">
+            Aer Global
+            </h1> 
             <video
                 autoPlay
                 loop

c3eca100441faa007051ffade0ba1ac476d44e8d
Yash Shirke <51786748+yash5201@users.noreply.github.com>
2022-10-24 00:09:28 +0530

    update 4.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import type { AppProps } from "next/app";
 import SizeObserver from "../utils/size-observer";
 import AniBall from "../components/AniBall";
 import Head from "next/head";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
@@ -25,7 +27,25 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <link rel="icon" href="/global.jpeg" />
         <title>ÆR Global &nbsp; &nbsp; &nbsp;</title>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
       </Head>
+
       {loading ? (
         <div className="flex h-screen items-center justify-center">
           <AniBall />
